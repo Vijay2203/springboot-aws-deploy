defcc9416b45a1e7b1bbb29a579ac373d99de770
Vijay2203 <61653266+Vijay2203@users.noreply.github.com>
2025-07-23 13:10:43 +0530

    Update TestController.java
    changes

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS Eks";
+        return "First message from AWS Ecr";
     }
 
     @GetMapping("/message")
